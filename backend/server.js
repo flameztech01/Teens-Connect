37f8d6ae05743759d789af5ec5b128208cc8a9d3
flameztech01 <flameztech01@gmail.com>
2026-08-15 10:11:44 +0100

    new

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,8 @@ const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/myapp';
 // Middleware
 const allowedOrigins = [
   'http://localhost:3000',
-  'https://teens-connect.vercel.app'
+  'https://teens-connect.vercel.app',
+  'https://teensconnect.flameztech.online'
 ];
 
 // ✅ CORS
